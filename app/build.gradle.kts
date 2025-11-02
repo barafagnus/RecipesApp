e8efce0ecc09fc7e82c4d07436f6744aca2600f1
barafagnus <barafagnus@gmail.com>
2025-11-02 23:52:39 +0700

    minSdk to 28, added launcher icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "ru.vysokov.recipesapp"
-        minSdk = 24
+        minSdk = 28
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
