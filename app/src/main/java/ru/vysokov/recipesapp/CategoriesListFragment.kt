976cac7d70a0146b306c467499ae255e61e11ed9
barafagnus <barafagnus@gmail.com>
2025-11-19 20:34:56 +0700

    RA-10 trans data to item recipe fragment

--- app/src/main/java/ru/vysokov/recipesapp/CategoriesListFragment.kt
+++ app/src/main/java/ru/vysokov/recipesapp/CategoriesListFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
@@ -44,19 +45,26 @@ class CategoriesListFragment : Fragment() {
 
         categoriesListAdapter.setOnItemClickListener(
             object : CategoriesListAdapter.OnItemClickListener {
-                override fun onItemClick() {
-                    openRecipesByCategoryId()
+                override fun onItemClick(categoryId: Int) {
+                    openRecipesByCategoryId(categoryId)
                 }
             }
         )
     }
 
-    private fun openRecipesByCategoryId() {
+    private fun openRecipesByCategoryId(categoryId: Int) {
+        val categoryName = STUB.getCategories().find { it.id == categoryId }?.title
+        val categoryImageUrl = STUB.getCategories().find { it.id == categoryId }?.imageUrl
+        val bundle = bundleOf(
+            "ARG_CATEGORY_ID" to categoryId,
+            "ARG_CATEGORY_NAME" to categoryName,
+            "ARG_CATEGORY_IMAGE_URL" to categoryImageUrl
+        )
+
         parentFragmentManager.commit {
-            replace<RecipesListFragment>(R.id.mainContainer)
+            replace<RecipesListFragment>(R.id.mainContainer, args = bundle)
             setReorderingAllowed(true)
             addToBackStack(null)
         }
     }
-
 }
